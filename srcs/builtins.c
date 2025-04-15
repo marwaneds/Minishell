1208299aed94e60b5aa6030b9397f637916cda62
Marwane El faquiri <dusman@MacBook-Air-de-Marwane.local>
2025-04-15 15:26:19 +0200

    fin

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -4,16 +4,16 @@ int	check_builtins(t_data *data, t_cmds *cmd, char *str)
 {
 	if (!ft_strcmp(str, "echo"))
 		return (builtin_echo(data, cmd));
-	else if (!ft_strcmp(str, "cd"))
-		return (builtin_cd(data, cmd));
 	else if (!ft_strcmp(str, "pwd"))
 		return (builtin_pwd(data, cmd));
-	else if (!ft_strcmp(str, "export"))
-		return (builtin_export(data, cmd));
+	else if (!ft_strcmp(str, "cd"))
+		return (builtin_cd(data, cmd));
 	else if (!ft_strcmp(str, "unset"))
 		return (builtin_unset(data, cmd));
 	else if (!ft_strcmp(str, "env"))
 		return (builtin_env(data, cmd));
+	else if (!ft_strcmp(str, "export"))
+		return (builtin_export(data, cmd));
 	else if (!ft_strcmp(str, "exit"))
 		return (builtin_exit(data, cmd));
 	return (0);
